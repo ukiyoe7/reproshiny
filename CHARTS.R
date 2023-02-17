f058ee54681c97321d26edd054a3e75662f3e398
Sandro Jk <sandro007@gmail.com>
2023-02-17 00:51:22 -0300

    ok

--- CHARTS.R
+++ CHARTS.R
@@ -10,9 +10,8 @@ con2 <- dbConnect(odbc::odbc(), "reproreplica", timeout = 10)
 
 ## METAS
 
-metas <- read.table(text = read_clip(), header = TRUE, sep = "\t") 
-
-save(metas,file = "app/metas2023.RData")
+## metas <- read.table(text = read_clip(), header = TRUE, sep = "\t") 
+## save(metas,file = "app/metas2023.RData")
 
 metas2023 <- get(load(file = "app/metas2023.RData"))
 
@@ -32,25 +31,26 @@ result %>% group_by(SETOR) %>% summarize(VRVENDA=sum(VRVENDA))
 portuguese_month_names <- c("janeiro", "fevereiro", "março", "abril", "maio", "junho",
                             "julho", "agosto", "setembro", "outubro", "novembro", "dezembro")
 
-
+## METAS
 metas2 <-  metas2023 %>%  melt(.,id.vars="SETOR") %>% 
     rename(MES=variable, META=value) %>% 
   
   mutate(MES = match(MES, portuguese_month_names)) %>% 
   mutate(MES =ymd(paste0("2023-", MES, "-01"))) %>% filter(month(MES) == month(Sys.Date())) %>% 
-
 mutate(META = gsub("[.]", "", META)) %>%
   mutate(META = as.numeric(META)) 
 
+## ESPERADO
 
+first_day <- floor_date(Sys.Date(), unit = "month")
 num_days <- as.numeric(difftime(Sys.Date(), first_day, units = "days"))
 
 
-
-first_day <- floor_date(Sys.Date(), unit = "month")
 num_days <- as.numeric(days_in_month(first_day))
 
-num_weekends <- sum(wday(seq(first_day, last_day, by = "day")) %in% c(1,7))
+
+
+num_weekends <- sum(wday(seq(first_day, ceiling_date(Sys.Date(),unit = "month")-1, by = "day")) %in% c(1,7))
 num_days = num_days - num_weekends -2
 
 num_days_2 <- as.numeric(difftime(Sys.Date(), first_day, units = "days"))
@@ -79,8 +79,10 @@ bullet_base_rep <-
   mutate(SETOR = sort(rep(result3$SETOR, 5) ))
 
 
-bullet_colors <- c("#c3c3c3", "#b4b4b4","#5e5e5e","#bfff00","#b4b4b4")
+bullet_colors <- c("#c3c3c3", "#b4b4b4","#8995a2","#7c0a02","#b4b4b4")
 names(bullet_colors) <- c("Ruim", "Regular","ok" ,"Bom", "Otimo")
+
+colors <- c("#560701", "#7c0a02")
 
 ggplot() +
   geom_bar(data = bullet_base_rep, 
@@ -92,23 +94,36 @@ ggplot() +
            stat = "identity") + 
   
   geom_bar(data = result3, 
-           aes(x = fct_rev(SETOR), y = ALCANCE , fill = "ALCANCE"), width = .5,
+           aes(x = fct_rev(SETOR), y = ALCANCE , fill = "ALCANCE"), width = .55,
            stat = "identity") + 
   
 
   
   geom_text(data = result3, aes(x = fct_rev(SETOR), y = ALCANCE, label = percent(round(ALCANCE,1)/100)), 
             position = position_dodge(width = .7), vjust = 0.5, hjust = -0.1,size=4.5) +
-  scale_fill_manual(values = c(bullet_colors, ESPERADO = "#c1cdcd", ALCANCE = "#1974d2")) +
+  scale_fill_manual(values = c(bullet_colors, ESPERADO = "#f4d27a", ALCANCE = "#1974d2"), 
+                    breaks = c("ESPERADO", "ALCANCE")) +
   coord_flip(expand = FALSE) + theme(axis.title.y=element_blank(),
                                      axis.text.y = element_text(size = 13)) +
   theme(panel.background = element_rect(fill = "transparent"),
         panel.grid = element_blank(),
         panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()) +
-  scale_y_continuous(breaks=c(0, 50, 70,90)) +
-  guides(fill = guide_legend(override.aes = list(fill = NULL)))
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 13)) +
   
+  theme(legend.position = "top", # adjust the legend position
+        legend.text = element_text(size = 14), # increase the font size of legend text
+        legend.title = element_blank(), # remove the legend title
+        axis.title.y = element_blank(), axis.text.y = element_text(size = 15))+ 
+  theme(
+        axis.text.y = element_text(size = 13))  +
+  
+  scale_y_continuous(breaks=c(0,50,90,100))
+  
+## FATURADO
+
+
 
 
 
